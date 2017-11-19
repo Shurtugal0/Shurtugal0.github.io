34ff3a6af5328843c9b155e561a31e044504a755
Sveshnikov Nikita <svesnikita@gmail.com>
2017-11-19 19:33:33 +0300

    Task3

---
+++ app.js
@@ -0,0 +1,35 @@
+$.fn.create_line = function(text){
+    this.append($('<span>')).text(text);
+    var delete_button = $('<input>').attr({
+        type: 'button',
+        value: 'Удалить'
+    });
+    $(delete_button).click(function(){
+        $(this).closest('li').remove();
+    });
+    this.append(delete_button);
+};
+
+$(function() {
+    var list = $('<ul>');
+    var elt = $('<li>');
+    elt.create_line('Сделать задание #3 по web-программированию');
+    var input = $('<input>').attr({
+        type: 'text',
+        id: 'add_task_input'
+    });
+    var add_button = $('<input>').attr({
+        type: 'button',
+        id: 'add_task',
+        value: 'Добавить'
+    });
+    $(add_button).click(function() {
+        var add = $('<li>');
+        add.create_line($('#add_task_input').val());
+        list.append(add);
+    });
+    list.append(elt);
+    $('#root').append(list);    
+    $('#root').append(input);
+    $('#root').append(add_button);
+});
